e5a43e560cc5111261baf424c9b0e9b3708c9fdc
joshtag <joseoatanha@gmail.com>
2022-06-04 16:12:22 +0100

    Refatorização 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
+// Change Navbar - with Vanilla js.
 const navbar = document.getElementById('navbar');
-
 window.onscroll = () => {
     if(window.scrollY > 689){
         navbar.classList.add('fixed-navbar')
@@ -8,3 +8,19 @@ window.onscroll = () => {
         navbar.classList.remove('fixed-navbar')
     }
 }
+
+// Smooth Scrolling - Using Jquery
+$("#navbar a").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top
+            },
+            800
+        );
+    }
+});
